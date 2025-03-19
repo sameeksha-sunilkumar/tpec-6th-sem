53618b615b04ef9c04c268f685b9ce67418fa448
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-03-19 23:49:38 +0530

    Update!!!!! regular 1st program

--- r_one.c
+++ r_one.c
@@ -1,51 +1,39 @@
 #include <stdio.h>
 
-void printArray(int arr[], int N) {
-	int i;
-    for ( i = 0; i < N; i++) {
-        printf("%d ", arr[i]);
+void print(int ar_size, int* ar) {
+    for(int i = 0; i < ar_size; i++) {
+        printf("%d ", ar[i]);
     }
     printf("\n");
 }
 
-void insertionSort(int arr[], int N) {
-	int i;
-    for ( i = 1; i < N; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
+void insertionSort(int ar_size, int* ar) {
+    for (int i = ar_size - 2; i >= 0; i--) {  // Start from second last element
+        int v = ar[i];  
+        int j = i + 1;
 
-        // Print array after each pass
-        printf("Step %d: ", i);
-        printArray(arr, N);
+        // Shift elements to the left if they are greater than v
+        while (j < ar_size && ar[j] < v) {
+            ar[j - 1] = ar[j];
+            j++;
+            print(ar_size, ar);  // Print after each shift
+        }
+
+        ar[j - 1] = v;  // Insert the element at the correct position
+        print(ar_size, ar);  // Print array after insertion
     }
 }
 
 int main() {
-    int N;
-    
-    printf("Enter the number of elements: ");
-    scanf("%d", &N);
+    int _ar_size;
+    scanf("%d", &_ar_size);
 
-    int arr[N];
-    int i;
-
-    printf("Enter %d elements: ", N);
-    for ( i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);
+    int _ar[_ar_size];
+    for (int i = 0; i < _ar_size; i++) { 
+        scanf("%d", &_ar[i]); 
     }
 
-    printf("Unsorted array: ");
-    printArray(arr, N);
-
-    insertionSort(arr, N);
-
-    printf("Sorted array: ");
-    printArray(arr, N);
-
-    return 0;
+    insertionSort(_ar_size, _ar);
+    
+    return 0;
 }
