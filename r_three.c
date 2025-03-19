5bd10834cfc15d939f9cdbcaaecaa71a07d60043
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-03-19 22:00:40 +0530

    regular 3rd program

---
+++ r_three.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void printArray(int arr[], int N) {
+    for (int i = 0; i < N; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int arr[], int N) {
+    for (int i = 1; i < N; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, N);
+    printArray(arr, N);  // Print the sorted array **after sorting**
+
+    return 0;
+}
