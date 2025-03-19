cb15d5b71a2f055aae4bf7c632b55917d81295e5
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-03-19 22:19:27 +0530

    additional 6th program

---
+++ a_six.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n, k, i, avail = 0, count = 0;
+    scanf("%d", &n);
+    scanf("%d", &k);
+    int cost[n]; 
+    for (i = 0; i < n; i++) {
+        scanf("%d", &cost[i]);
+    }
+    insertionSort(cost, n);
+    while (avail + cost[count] <= k && count < n) {
+        avail += cost[count];
+        count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}
