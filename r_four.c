c8c6696941f01c8dd2c16f433069ea157ac37099
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-03-20 00:59:36 +0530

    Updated regular 4th program

--- r_four.c
+++ r_four.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 
-void insertionSort(int arr[], int N, int *shift) {
-    *shift = 0;
-    for (int i = 1; i < N; i++) {
+int runningTime(int n, int arr[]) {
+    int shifts = 0;
+
+    for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
+
         while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-            (*shift)++;
+            arr[j + 1] = arr[j]; 
+            j--;
+            shifts++; 
         }
-        arr[j + 1] = key;
+
+        arr[j + 1] = key; 
     }
+
+    return shifts;
 }
 
 int main() {
-    int N;
-    
-    scanf("%d", &N);
+    int n;
+    scanf("%d", &n);
+    int arr[n];
 
-    int arr[N];
-    
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    int shift = 0;
-
-
-    insertionSort(arr, N, &shift);
-
-    printf("%d\n", shift);
+    printf("%d\n", runningTime(n, arr));
 
     return 0;
 }
