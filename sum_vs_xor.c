a1a52bd7ee3e6f7b6d6e73d298e9908bef2b1edf
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-06-23 19:57:31 +0530

    Update sum_vs_xor.c

--- sum_vs_xor.c
+++ sum_vs_xor.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
-
-long countZeroBits(long n) {
-    if (n == 0) return 1; // Edge case: n = 0, only x = 0 works, so answer is 1.
-    
-    int count = 0;
-    while (n) {
-        if ((n & 1) == 0) count++;  // Count zero bits
-        n >>= 1;  // Right shift n
-    }
-    return (1L << count);  // 2^count
-}
-
-int main() {
-    long n;
-    scanf("%ld", &n);
-    printf("%ld\n", countZeroBits(n));
-    return 0;
-}
+#include <stdio.h> 
+int main(){ 
+long long int n,m=1; 
+scanf("%lld",&n); 
+while(n>0){ 
+if(n%2==0)m*=2; 
+n/=2; 
+} 
+printf("%lld\n",m); 
+return 0; }
